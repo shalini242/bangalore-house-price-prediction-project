91ec5a3e0658904c943c2bf52fd5e4057f0107c1
shalini242 <150043632+shalini242@users.noreply.github.com>
2024-04-22 19:07:00 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, render_template, request
+import pickle
+from flask import jsonify
+
+app = Flask(__name__)
+data = pd.read_csv(r'C:\Users\shali\bhpp new\Bengaluru_House_Price_Prediction_Model-main\Cleaned_data.csv')
+pipe = pickle.load(open(r"C:\Users\shali\bhpp new\Bengaluru_House_Price_Prediction_Model-main\RidgeModel.pk1",'rb'))
+@app.route('/')
+def index():
+
+    locations = sorted(data['location'].unique())
+    return render_template('index.html', locations=locations)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    location = request.form.get('location')
+    bhk = request.form.get('bhk')
+    bath = request.form.get('bath')
+    sqft = request.form.get('total_sqft')
+
+    print(location, bhk, bath, sqft)
+
+    input = pd.DataFrame([[location,sqft,bath,bhk]],columns=['location', 'total_sqft', 'bath', 'bhk'])
+    prediction = pipe.predict(input)[0] * 1e5
+
+    return str(np.round(prediction,2))
+
+if __name__=="__main__":
+    app.run(debug=True, port=5001)
